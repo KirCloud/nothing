929b83db09a64afbfadadf6b265b5e2797317fb9
KirCloud <li1322958826@gmail.com>
2025-10-20 17:49:03 +0800

    delete private

--- app/Utils/Dict.php
+++ app/Utils/Dict.php
@@ -23,19 +23,19 @@ class Dict
     // 营销邮件系统默认配置
 CONST MARKETING_EMAIL_DEFAULT_CONFIG = [
     'marketing_email_enable' => 1,
-    'marketing_email_host' => 'smtp.qiye.aliyun.com',
+    'marketing_email_host' => 'smtp.com',
     'marketing_email_port' => 465,
     'marketing_email_encryption' => 'ssl',
-    'marketing_email_username' => 'kirssr@kirssr.fun',
-    'marketing_email_password' => 'Lizijie123456123123',
-    'marketing_email_from_address' => 'kirssr@kirssr.fun',
-    'marketing_email_from_name' => 'KirSSR',
+    'marketing_email_username' => '',
+    'marketing_email_password' => '',
+    'marketing_email_from_address' => '',
+    'marketing_email_from_name' => '',
     'marketing_email_frequency' => 'daily',
     'marketing_email_send_count' => 20,
     'marketing_email_user_limit' => 2,
     'marketing_email_time_limit' => 5,
-    'marketing_email_subject' => 'KirSSR欢迎您回归，特奉上8折优惠码',
-    'marketing_email_brand_name' => 'KirSSR',
-    'marketing_email_coupon_code' => 'WelcomeBack',
+    'marketing_email_subject' => '欢迎您回归，特奉上8折优惠码',
+    'marketing_email_brand_name' => '',
+    'marketing_email_coupon_code' => '',
 ];
 }
